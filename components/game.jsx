1e9897cc1feedd5d526b3dcad71ee2c13e2cd497
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-08 10:41:08 -0800

    Setup project

--- components/game.jsx
+++ components/game.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import * as Minesweeper from '../minesweeper';
 
 class Game extends React.Component{
+  constructor() {
+    this.state = {
+      board: new Minesweeper.Board()
+    };
+  }
 
+  updateGame() {
+
+  }
+
+  render() {
+    
+  }
 }
 
 export default Game;
